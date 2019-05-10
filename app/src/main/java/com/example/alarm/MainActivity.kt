f66eda610e7f18c4bdc3c3a9eb6372ca03bc37b6
Kuba <kyuuba97@gmail.com>
2019-05-10 12:13:42 +0200

    Added login and registration activities. Connected to the server.

--- app/src/main/java/com/example/alarm/MainActivity.kt
+++ app/src/main/java/com/example/alarm/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.alarm
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.parse.ParseInstallation
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ParseInstallation.getCurrentInstallation().saveInBackground()
+    }
+
+    fun goToRegister(view : View){
+        var intent = Intent(this, Register::class.java)
+        startActivity(intent)
+
     }
 }
