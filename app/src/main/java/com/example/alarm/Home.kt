f494733c0a62e91a945fcb47576a0293735fd598
dkunc <d.kunc@op.pl>
2019-05-21 21:32:47 +0200

    Added sending push notifications to users followers with specified message and title.

--- app/src/main/java/com/example/alarm/Home.kt
+++ app/src/main/java/com/example/alarm/Home.kt
@@ -19,22 +19,46 @@ class Home : AppCompatActivity() {
     }
 
     fun test(view : View){
-        val parameters = HashMap<String, String>()
-        var channels = "JxAZ6fqOCN CH8txr6UJj"
-        parameters.put("channels", channels)
-        ParseCloud.callFunctionInBackground("pushsample", parameters,
-            FunctionCallback<String> { followers, e ->
+        val parametersForFollowers = HashMap<String, String>()
+        parametersForFollowers.put("user", ParseUser.getCurrentUser().objectId.toString())
+        ParseCloud.callFunctionInBackground("getFollowers", parametersForFollowers,
+            FunctionCallback<ArrayList<Any>> { followers, e ->
                 if (e == null) {
-                    var message = "lol"
-                    /*for(i in 0 until followers.size){
-                        message +=followers[i].toString()+";"
-                    }*/
-                    Toast.makeText(this@Home, message, Toast.LENGTH_LONG).show()
+                    var channels = ""
+                    for(i in 0 until followers.size) {
+                        var jsonArray = JSONArray(followers.toString())
+                        for(j in 0 until jsonArray.length() )
+                        {
+                            channels += jsonArray.getJSONObject(j).get("id").toString()+" "
+
+                        }
+                    }
+                    System.out.println(channels)
+                    val parameters = HashMap<String, String>()
+
+                    parameters.put("channels", channels)
+                    parameters.put("message", "Kuba śmierdzi")
+                    parameters.put("title", "SEKRET")
+
+                    ParseCloud.callFunctionInBackground("pushsample", parameters,
+                        FunctionCallback<String> { followers, e ->
+                            if (e == null) {
+                                var message = "lol"
+                                /*for(i in 0 until followers.size){
+                                    message +=followers[i].toString()+";"
+                                }*/
+                                Toast.makeText(this@Home, message, Toast.LENGTH_LONG).show()
+                            } else {
+                                // Something went wrong
+                                Toast.makeText(this@Home, "Oh no: " + e.toString(), Toast.LENGTH_LONG).show()
+                            }
+                        })
                 } else {
                     // Something went wrong
                     Toast.makeText(this@Home, "Oh no: " + e.toString(), Toast.LENGTH_LONG).show()
                 }
             })
+
         /*var message = "Wiadomość: "
         val query = ParseUser.getQuery()
         query.whereEqualTo("email", ParseUser.getCurrentUser().email)
